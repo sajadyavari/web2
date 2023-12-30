33c85b7472257e1f80701cf03d3b89bc28e33b1f
asus <sajad2ser@gmail.com>
2023-12-30 18:33:04 +0330

    web2 7

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -6,11 +6,13 @@ st.header("Contact Me!")
 
 with st.form(key="contact-us"):
     email = st.text_input("Enter Your E-Mail Address")
+    topic = st.selectbox("Topic", ("first", "second", "third"))
     raw_message = st.text_area("Your Message")
     message = f"""\
 Subject: Contact From {email}
 
 From: {email}
+Topic: {topic}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
