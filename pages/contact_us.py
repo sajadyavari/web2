5e733ee9037113c74df030f845963fde3c5fff32
asus <sajad2ser@gmail.com>
2023-12-30 18:18:56 +0330

    web2 6

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header("Contact Me!")
+
+with st.form(key="contact-us"):
+    email = st.text_input("Enter Your E-Mail Address")
+    raw_message = st.text_area("Your Message")
+    message = f"""\
+Subject: Contact From {email}
+
+From: {email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("Email sent Now!")
---
+++ send_email.py
@@ -0,0 +1,14 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    user_name = "sajadyavari.ir@gmail.com"
+    password = "ryzaznzpxjozrlnr"
+    receiver_email = "sajad2ser@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver_email, message)
